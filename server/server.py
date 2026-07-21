338525a4d1bce4c23a3933b7090a17fdff3df1bb
Dinh Bao Binh Tran <trandinhbaobinh2005@aobinh2005gmail.h2005gmailcom>
2026-07-21 16:24:33 -0500

    ok

--- server/server.py
+++ server/server.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util
 
 app = Flask(__name__)
+# Khởi tạo CORS cho toàn bộ ứng dụng
+CORS(app)
+
+# Load sẵn artifacts ngay khi server khởi động trên Render / Gunicorn
+util.load_saved_artifacts()
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -20,13 +24,10 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
     app.run()
