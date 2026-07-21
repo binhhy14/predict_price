7c20032ffcef298260a33272ae269b51ea3db207
Dinh Bao Binh Tran <trandinhbaobinh2005@aobinh2005gmail.h2005gmailcom>
2026-07-21 16:11:41 -0500

    update

--- client/app.js
+++ client/app.js
@@ -26,8 +26,8 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  // Nếu dùng Live Server (5500) kết nối tới Flask (5000):
-  var url = "http://127.0.0.1:5000/predict_home_price";
+  // Đã cập nhật sang URL API trên Render
+  var url = "https://predict-price-psro.onrender.com/predict_home_price";
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -43,7 +43,8 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log("document loaded");
-  var url = "http://127.0.0.1:5000/get_location_names";
+  // Đã cập nhật sang URL API trên Render
+  var url = "https://predict-price-psro.onrender.com/get_location_names";
   
   $.get(url, function(data, status) {
       console.log("got response for get_location_names request");
